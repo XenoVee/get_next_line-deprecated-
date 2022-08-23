32091d5fff675127ed275cebf7a1a68f9f8d3e8b
rmaes <ikbenruudmaes@gmail.com>
2022-08-23 15:36:05 +0200

    aaaaaaaaa

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rmaes <rmaes@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:57:57 by rmaes             #+#    #+#             */
-/*   Updated: 2022/08/02 20:33:02 by rmaes            ###   ########.fr       */
+/*   Updated: 2022/08/23 15:16:21 by rmaes            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@
 # include <unistd.h>
 
 char	*extend_malloc(char	*str, int size, int ext);
-int		linelen(char *str);
 char	*get_next_line(int fd);
 void	*ft_calloc(unsigned long count, unsigned long size);
 void	*ft_bzero(void *e, size_t len);
